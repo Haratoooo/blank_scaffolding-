05727e09ef4c1a043be0544e37c42c2e8eb97ac9
Heart Alvern Sumicad <145334591+Haratoooo@users.noreply.github.com>
2024-06-23 22:36:18 +0800

    Update login.vue

--- resources/js/components/forms/login.vue
+++ resources/js/components/forms/login.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
     <b-table striped hover :items="items" :fields="fields"></b-table>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="button" variant="primary" @click="register()">Sign Up</b-button>
+    </b-form>
   </div>
 </template>
 
